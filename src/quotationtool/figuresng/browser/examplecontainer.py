080ac3f59e2f427d4cf0308626546938a6ef77d5
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2011-03-31 23:22:15 +0200

    Added a search form primer for accessing by ID

--- src/quotationtool/figuresng/browser/examplecontainer.py
+++ src/quotationtool/figuresng/browser/examplecontainer.py
@@ -1,7 +1,15 @@
 import zope.component
+import zope.interface
+import zope.schema
 from z3c.pagelet.browser import BrowserPagelet
 from zope.publisher.browser import BrowserView
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from z3c.form.form import Form as ViewletForm
+from z3c.form import field, button
+from zope.viewlet.interfaces import IViewlet
+from zope.traversing.browser.absoluteurl import absoluteURL
+from zope.app.component.hooks import getSite
+from zope.traversing.api import traverse
 
 from quotationtool.search.browser import search
 
@@ -58,3 +66,48 @@ class SearchResultPage(search.SearchResultPageBase):
 
     def getExamples(self):
         return self.getResultPage()
+
+
+example_id = zope.schema.TextLine(
+    title = u"ID",
+    required = False,
+    )
+example_id.__name__ = 'example_id'
+
+
+class ExampleByIdForm(ViewletForm):
+
+    label = _('example-by-id-label',
+              u"Example by ID (unique Number)")
+
+    zope.interface.implements(IViewlet)
+
+    ignoreContext = True
+
+    prefix = "examplebyid."
+
+    fields = field.Fields(example_id)
+
+    def __init__(self, context, request, view, manager):
+        """ See zope.viewlet.viewlet.ViewletBase"""
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.manager = manager
+
+    @button.buttonAndHandler(_(u"Show!"), name = 'show')
+    def handleShow(self, action):
+        data, errors = self.extractData()
+        if not data['example_id'] in self.context:
+            self.status = _('example-by-id-failure',
+                            u"Example #$ID does not exist",
+                            mapping = {'ID': data['example_id']})
+            return
+        container = zope.component.getUtility(
+            iexample.IExampleContainer,
+            context = getSite(),
+            )
+        self.request.response.redirect(
+            absoluteURL(container, self.request) 
+            + u"/" + data['example_id']
+            )
