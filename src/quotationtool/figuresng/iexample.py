6ca357178928901d4f0c2266695c8815a9a8e25b
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2011-07-03 14:03:15 +0200

    Fixed interface that was removed to early

--- src/quotationtool/figuresng/iexample.py
+++ src/quotationtool/figuresng/iexample.py
@@ -1,2 +1,14 @@
+# needed for ZODB for historical reasons
 from interfaces import *
 
+#BBB: to be removed in 1.0
+from zope.interface import Interface, Attribute
+class IExampleIndexCatalog(Interface):
+    """A collection of indexes for example objects. """
+
+    quid = Attribute('BBB')
+    pro_quo = Attribute('BBB')
+    quid_pro_quo = Attribute('BBB')
+    marker = Attribute('BBB')
+    quotation = Attribute('BBB')
+    any = Attribute('BBB')
