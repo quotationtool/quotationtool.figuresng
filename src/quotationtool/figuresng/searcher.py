5fb88330c396f4fc3f6a57a7c3d527f8a3432d4c
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2011-07-03 18:11:32 +0200

    Improved search UI

--- src/quotationtool/figuresng/searcher.py
+++ src/quotationtool/figuresng/searcher.py
@@ -8,6 +8,7 @@ from zope.traversing.browser import absoluteURL
 
 from quotationtool.search.interfaces import IQuotationtoolSearchFilter
 from quotationtool.search.interfaces import ITypeExtent, ICriteriaChainSpecifier, IResultSpecifier
+from quotationtool.search.interfaces import ICriteriumDescription
 from quotationtool.quotation.interfaces import IQuotationSearchFilter
 
 from quotationtool.figuresng.interfaces import _, IExampleSearchFilter, IExampleContainer
@@ -56,9 +57,15 @@ example_search_filter_factory = zope.component.factory.Factory(
 class QuidTextCriterium(TextCriterium):
     """ Full text criterium for 'quid-fulltext' index."""
 
+    zope.interface.implements(ICriteriumDescription)
+
     indexOrName = 'quid-fulltext'
     
     label = _('quid-fulltext-label', u"Example")
+
+    description = _('quid-fulltext-desc', u"Matches in example field.")
+
+    ui_weight = 15
 
 quid_fulltext_factory = factory(QuidTextCriterium, 'quid-fulltext')
 
@@ -66,9 +73,15 @@ quid_fulltext_factory = factory(QuidTextCriterium, 'quid-fulltext')
 class ProquoTextCriterium(TextCriterium):
     """ Full text criterium for 'proquo-fulltext' index."""
 
+    zope.interface.implements(ICriteriumDescription)
+
     indexOrName = 'proquo-fulltext'
     
     label = _('proquo-fulltext-label', u"Denotation / Meaning")
+
+    description = _('proquo-fulltext-desc', u"Matches in denotation or meaning field.")
+
+    ui_weight = 20
 
 proquo_fulltext_factory = factory(ProquoTextCriterium, 'proquo-fulltext')
 
@@ -76,8 +89,14 @@ proquo_fulltext_factory = factory(ProquoTextCriterium, 'proquo-fulltext')
 class MarkerTextCriterium(TextCriterium):
     """ Full text criterium for 'marker-fulltext' index."""
 
+    zope.interface.implements(ICriteriumDescription)
+
     indexOrName = 'marker-fulltext'
     
     label = _('marker-fulltext-label', u"Marker")
+
+    description = _('marker-fulltext-desc', u"Matches in marker field.")
+
+    ui_weight = 30
 
 marker_fulltext_factory = factory(MarkerTextCriterium, 'marker-fulltext')
